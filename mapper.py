4e92237dd391cbf969799ddb8321898f57523be8
Tom JEANNESSON <tomjeannesson@gmail.com>
2024-11-14 11:17:13 +0100

    tmp

--- mapper.py
+++ mapper.py
@@ -1,6 +1,14 @@
+#! /usr/bin/env python3
+
 import sys
+
+SEPARATOR = "\t"
 
 if __name__ == "__main__":
     data = sys.stdin.read()
-    for word in data.split(" "):
-        print(f"{word} 1")
+    for char in ["--", "(", ")", ".", ",", ";", ":", "?", "!", "»", "«", "_", "«", "»"]:
+        data = data.replace(char, " ")
+    for word in data.lower().replace("\n", " ").split(" "):
+        if word not in  ("", "\t", "\n"):
+            print(word + SEPARATOR + "1")
+
--- reducer.py
+++ reducer.py
@@ -1,12 +1,15 @@
+#! /usr/bin/env python3
+
 import sys
+SEPARATOR = "\t"
 
 if __name__ == "__main__":
     data = sys.stdin.read()
-    all_vales = {}
+    all_values = {}
     for w in data.split("\n"):
         if w == "":
             continue
-        word, count = w.split(" ")
-        all_vales[word] = all_vales.get(word, 0) + int(count)
-    for word, count in all_vales.items():
-        print(f"{word} {count}")
+        word, count = w.split(SEPARATOR)
+        all_values[word] = all_values.get(word, 0) + int(count)
+    for word, count in sorted(all_values.items()):
+        print(word + SEPARATOR + str(count))
